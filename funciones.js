65d3c8e2b7d7fa4dc4efdf8e895215abf9df1cd6
HJSP <78765268+HannahSolis@users.noreply.github.com>
2023-04-04 22:45:00 -0500

    implemented initial extend and contract function

---
+++ funciones.js
@@ -0,0 +1,41 @@
+// document.querySelector(".n1l").classList.add("hide"); 
+
+// document.querySelector(".n1l").classList.remove("hide"); 
+
+//document.querySelector(".n1").classList.add("extend");
+
+//document.querySelector(".n1").classList.remove("extend");
+
+//document.querySelector(".n1 > div").classList.add("extend");
+
+//document.querySelector(".n1 > div").classList.remove("extend");
+
+function extender(btn) {
+    const derecha ="#" +btn.id;
+    console.log(derecha);
+    const izquierda ="." +btn.id +"l";
+    console.log(izquierda);
+    const derExt = "#" +btn.id +" > div";
+    console.log(derExt);
+    
+    document.querySelector(izquierda).classList.add("hide"); 
+
+    document.querySelector(derecha).classList.add("extend");
+    document.querySelector(derExt).classList.add("extend");
+
+    
+}
+
+
+function contraer(btn) {
+    const derecha ="#" +btn.id;
+    console.log(derecha);
+    const izquierda ="." +btn.id +"l";
+    console.log(izquierda);
+    const derExt = "#" +btn.id +" > div";
+    console.log(derExt);
+
+    document.querySelector(izquierda).classList.remove("hide"); 
+    document.querySelector(derecha).classList.remove("extend");
+    document.querySelector(derExt).classList.remove("extend");
+}
